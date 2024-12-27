e30106a1eb94807e66ffe1c0c01aea99ec95887a
Teza01x <110857514+teza01x@users.noreply.github.com>
2024-12-27 15:47:28 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
 import os
 
 
-telegram_token = '6848614344:AAHEIiSRnCcfQqHy4CpfEUFswWRFPVWNNOE'
+telegram_token = ''
 path_to_browser = "/usr/bin/google-chrome"
 limit_queue = 3
 
-private_group_id = -1001888476288
+private_group_id = -1111111
 
 data_base = os.path.join(os.path.dirname(__file__), 'database.db')
